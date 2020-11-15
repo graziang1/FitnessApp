92933d3d363dba23d4ea55c56c9b3c68ef832a0e
Gianna Graziano <graziang1@hawkmail.newpaltz.edu>
2020-11-15 01:21:41 -0500

    created register function

--- sever/controllers/users.js
+++ sever/controllers/users.js
@@ -32,6 +32,18 @@ router
             res.send( newUser ); //returns new user
         }).catch(next)
     })
+    .post('/register', (req, res, next) => {
+        users.register(
+            req.body.FirstName,
+            req.body.LastName, 
+            req.body.DOB, 
+            req.body.Password, 
+            users.Types.USER,
+            req.body.Email 
+        ).then(newUser => {
+            res.send( newUser );
+        }).catch(next)
+    })
     .put('/:id', (req, res, next) => {
         users.update( req.params.id,
             req.body.FirstName, 
